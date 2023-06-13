8fb84257c9f6c1c73a2633c19766f5a129d554c7
illkkeerr <ilkersenel5797@gmail.com>
2023-06-13 21:08:06 +0300

    Asp.Net_Mvc_E_Ticaret_Sitesi
    Projede katmanlı mimari kullanılmıştır.

---
+++ E-Shop_1/DataAccessLayer/Context/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Instrumentation;
+using System.Text;
+using System.Threading.Tasks;
+using EntityLayer.Entities;
+using System.Configuration;
+using System.Runtime.Remoting.Contexts;
+using System.Data.Entity;
+
+namespace DataAccessLayer.Context
+{
+    public class DataContext : DbContext
+    {
+
+
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Sales> Sales { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ E-Shop_1/EntityLayer/Entity/Product.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace EntityLayer.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Boş geçilemez.")]
+        [Display(Name = "Açıklama")]
+        [StringLength(100, ErrorMessage = "Max 100 karakter olmalıdır.")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Boş geçilemez.")]
+        [Display(Name = "Fiyat")]
+        public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "Boş geçilemez.")]
+        [Display(Name = "Stok")]
+        public int Stok { get; set; }
+
+        [Required(ErrorMessage = "Boş geçilemez.")]
+        [Display(Name = "Popüler")]
+        public bool Popular { get; set; }
+
+        [Required(ErrorMessage = "Boş geçilemez.")]
+        [Display(Name = "Onay")]
+        public bool IsApproved { get; set; }
+
+        [Required(ErrorMessage = "Boş geçilemez.")]
+        [Display(Name = "Resim")]
+        public string Image { get; set; }
+
+        [Required(ErrorMessage = "Boş geçilemez.")]
+        [Display(Name = "Adet")]
+        public int Quantity { get; set; }
+
+        [Required(ErrorMessage = "Boş geçilemez.")]
+        [Display(Name = "Kategori")]
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
